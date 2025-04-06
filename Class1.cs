e68ef086ca96d5c9931c9ad69d3111ec3c152de1
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-06 17:33:40 -0300

    Fix Pet Flag hopefully

--- Class1.cs
+++ Class1.cs
@@ -2905,9 +2905,22 @@ namespace SlayTheFrost
     [HarmonyPatch(typeof(PetHutFlagSetter), "SetupFlag")]
     internal static class PatchInPetFlag
     {
-        static void Prefix(PetHutFlagSetter __instance)
+        static Sprite louseFlag;
+        static void Postfix(PetHutFlagSetter __instance)
         {
-            __instance.flagSprites = __instance.flagSprites.Append(MainModFile.instance.ImagePath("LouseFlag.png").ToSprite()).ToArray();
+            if (louseFlag == null)
+            {
+                Texture2D louseTex = MainModFile.instance.ImagePath("LouseFlag.png").ToTex();
+                louseFlag = Sprite.Create(louseTex, new Rect(0f, 0f, louseTex.width, louseTex.height), new Vector2(0.5f, 1.0f), 160);
+            }
+
+            int petIndex = SaveSystem.LoadProgressData("selectedPet", 0);
+            string[] petInfo = MetaprogressionSystem.GetUnlockedPets();
+
+            if (petIndex < petInfo.Length && petInfo[petIndex].Equals(Extensions.PrefixGUID("louse", MainModFile.instance)))
+        {
+                __instance.flag.sprite = louseFlag;
+            }
         }
     }
 
