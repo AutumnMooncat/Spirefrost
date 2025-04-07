46e94b505ad7a007c5c254dd9c459d1dc7ce5a13
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-07 03:21:06 -0300

    Fix Mark CME

--- SpirefrostStatusEffects.cs
+++ SpirefrostStatusEffects.cs
@@ -249,24 +249,30 @@ namespace Spirefrost
             // All enemies with Mark lose hp
             foreach (Entity entity in Battle.GetAllUnits(Battle.GetOpponent(GetDamager().owner)))
             {
+                int markAmount = 0;
                 foreach (StatusEffectData effect in entity.statusEffects)
                 {
                     if (effect is StatusEffectSTSMark && effect.count > 0)
                     {
-                        // Hit em
-                        Hit hit = new Hit(GetDamager(), entity, effect.count)
-                        {
-                            screenShake = 0.25f,
-                            canRetaliate = false,
-                        };
-                        // Add VFX Later?
-                        //var transform = entity.transform;
-                        //VFXMod.instance.VFX.TryPlayEffect("stsmark", transform.position, transform.lossyScale);
-                        // Add SFX Later?
-                        //VFXMod.instance.SFX.TryPlaySound("stsmark");
-                        yield return hit.Process();
-                        yield return Sequences.Wait(0.2f);
+                        markAmount += effect.count;
+                        
                     }
+                }
+                if (markAmount > 0)
+                {
+                    // Hit em
+                    Hit hit = new Hit(GetDamager(), entity, markAmount)
+                    {
+                        screenShake = 0.25f,
+                        canRetaliate = false,
+                    };
+                    // Add VFX Later?
+                    //var transform = entity.transform;
+                    //VFXMod.instance.VFX.TryPlayEffect("stsmark", transform.position, transform.lossyScale);
+                    // Add SFX Later?
+                    //VFXMod.instance.SFX.TryPlaySound("stsmark");
+                    yield return hit.Process();
+                    yield return Sequences.Wait(0.2f);
                 }
             }
         }
