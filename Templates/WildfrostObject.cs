19ea63e5e6204c95a88d5328b91d6c94a5f74def
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-17 22:52:46 -0300

    Create builder template

---
+++ Templates/WildfrostObject.cs
@@ -0,0 +1,16 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+
+namespace Spirefrost.Templates
+{
+    internal class WildfrostObject : SpirefrostBuilder
+    {
+        internal static string ID => null;
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return null;
+        }
+    }
+}
