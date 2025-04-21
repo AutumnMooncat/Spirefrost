8f92f64a7f4d95e68d82151f1cb29c22b386bd6a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-21 14:30:57 -0300

    Add Lightning status

---
+++ Builders/Keywords/LightningKeyword.cs
@@ -0,0 +1,24 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using UnityEngine;
+
+namespace Spirefrost.Builders.Keywords
+{
+    internal class LightningKeyword : SpirefrostBuilder
+    {
+        internal static string ID => "stslightning";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new KeywordDataBuilder(MainModFile.instance)
+                .Create(ID)
+                .WithTitle("Lightning Orb")
+                .WithDescription("Deals damage to a random enemy every turn | Clears after triggering")
+                .WithTitleColour(new Color(0.95f, 0.95f, 0.05f))
+                .WithBodyColour(new Color(1.0f, 1.0f, 1.0f))
+                .WithNoteColour(new Color(0.94f, 0.94f, 0.04f))
+                .WithCanStack(false);
+        }
+    }
+}
---
+++ Builders/StatusEffects/IconEffects/Lightning.cs
@@ -0,0 +1,41 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.Icons;
+using UnityEngine;
+using WildfrostHopeMod.VFX;
+
+namespace Spirefrost.Builders.StatusEffects.IconEffects
+{
+    internal class Lightning : SpirefrostBuilder
+    {
+        internal static string ID => "STS Lightning";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new StatusEffectDataBuilder(MainModFile.instance)
+                .Create<StatusEffectOrb>(ID)
+                .WithCanBeBoosted(false)
+                .WithStackable(false)
+                .WithIsStatus(true)
+                .SubscribeToAfterAllBuildEvent<StatusEffectOrb>(data =>
+                {
+                    data.dealDamage = true;
+                    data.applyToFlags = StatusEffectApplyX.ApplyToFlags.RandomEnemy;
+                    data.targetConstraints = new TargetConstraint[]
+                    {
+                        ScriptableObject.CreateInstance<TargetConstraintCanBeBoosted>(),
+                        MakeConstraint<TargetConstraintOr>(or =>
+                        {
+                            or.constraints = new TargetConstraint[]
+                            {
+                                MakeConstraint<TargetConstraintMaxCounterMoreThan>(c => c.moreThan = 0),
+                                MakeConstraint<TargetConstraintHasReaction>()
+                            };
+                        })
+                    };
+                })
+                .Subscribe_WithStatusIcon(LightningIcon.ID);
+        }
+    }
+}
