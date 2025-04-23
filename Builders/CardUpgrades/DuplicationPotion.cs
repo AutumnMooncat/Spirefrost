486bb69df1a7b8653a39a0b02ff18e68d48f3d3a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-23 13:11:15 -0300

    Move DuplicationLogic to the actual DuplicationPotion class

--- Builders/CardUpgrades/DuplicationPotion.cs
+++ Builders/CardUpgrades/DuplicationPotion.cs
@@ -1,4 +1,5 @@
 ﻿using Deadpan.Enums.Engine.Components.Modding;
+using System.Collections;
 using UnityEngine;
 using static Spirefrost.MainModFile;
 using static Spirefrost.SpirefrostUtils.AutoAdd;
@@ -31,7 +32,7 @@ namespace Spirefrost.Builders.CardUpgrades
                             // Safety Lock
                             MainModFile.instance.looping = true;
 
-                            System.Collections.IEnumerator logic = SpirefrostUtils.DuplicationLogic(data, card);
+                            IEnumerator logic = DuplicationLogic(data, card);
 
                             while (logic.MoveNext())
                             {
@@ -50,6 +51,43 @@ namespace Spirefrost.Builders.CardUpgrades
                     };
                     //data.takeSlot = false;
                 });
+        }
+
+        internal static IEnumerator DuplicationLogic(CardUpgradeData dupeData, CardData cardData)
+        {
+            //Create Card Clone
+            CardData cloneData = cardData.Clone(runCreateScripts: false);
+            cloneData.upgrades.RemoveAll((CardUpgradeData a) => a.type == CardUpgradeData.Type.Crown);
+            dupeData.Clone().Assign(cloneData);
+
+            // Add Data To Inventory
+            Debug.Log("Duplication Potion → adding [" + cloneData.name + "] to " + References.Player.name + "'s deck");
+            References.Player.data.inventory.deck.Add(cloneData);
+
+            // Add To Sequence
+            GameObject deckDisplayObject = GameObject.Find("Canvas/Padding/PlayerDisplay/DeckDisplay");
+            DeckDisplaySequence sequence = deckDisplayObject.GetComponent<DeckDisplaySequence>();
+            Card card = CardManager.Get(cloneData, sequence.activeCardsGroup.cardController, null, inPlay: false, isPlayerCard: true);
+            sequence.activeCardsGroup.AddCard(card);
+
+            // Update Card
+            Routine.Clump clump = new Routine.Clump();
+            clump.Add(card.UpdateData());
+            //clump.Add(Sequences.Wait(10));
+            yield return clump.WaitForEnd();
+            sequence.UpdatePositions();
+
+            // Invoke Enter Backpack
+            Events.InvokeEntityEnterBackpack(card.entity);
+
+            CardContainer[] containers = card.entity.containers;
+            for (int i = 0; i < containers.Length; i++)
+            {
+                containers[i].TweenChildPositions();
+            }
+
+            // Remove Lock
+            MainModFile.instance.looping = false;
         }
     }
 }
--- SpirefrostUtils.cs
+++ SpirefrostUtils.cs
@@ -184,42 +184,5 @@ namespace Spirefrost
             Debug.Log($"FindEnumeratorMethod - Found Method: {moveNext}");
             return moveNext;
         }
-
-        internal static IEnumerator DuplicationLogic(CardUpgradeData dupeData, CardData cardData)
-        {
-            //Create Card Clone
-            CardData cloneData = cardData.Clone(runCreateScripts: false);
-            cloneData.upgrades.RemoveAll((CardUpgradeData a) => a.type == CardUpgradeData.Type.Crown);
-            dupeData.Clone().Assign(cloneData);
-
-            // Add Data To Inventory
-            Debug.Log("Duplication Potion → adding [" + cloneData.name + "] to " + References.Player.name + "'s deck");
-            References.Player.data.inventory.deck.Add(cloneData);
-
-            // Add To Sequence
-            GameObject deckDisplayObject = GameObject.Find("Canvas/Padding/PlayerDisplay/DeckDisplay");
-            DeckDisplaySequence sequence = deckDisplayObject.GetComponent<DeckDisplaySequence>();
-            Card card = CardManager.Get(cloneData, sequence.activeCardsGroup.cardController, null, inPlay: false, isPlayerCard: true);
-            sequence.activeCardsGroup.AddCard(card);
-
-            // Update Card
-            Routine.Clump clump = new Routine.Clump();
-            clump.Add(card.UpdateData());
-            //clump.Add(Sequences.Wait(10));
-            yield return clump.WaitForEnd();
-            sequence.UpdatePositions();
-
-            // Invoke Enter Backpack
-            Events.InvokeEntityEnterBackpack(card.entity);
-            
-            CardContainer[] containers = card.entity.containers;
-            for (int i = 0; i < containers.Length; i++)
-            {
-                containers[i].TweenChildPositions();
-            }
-
-            // Remove Lock
-            MainModFile.instance.looping = false;
-        }
     }
 }
