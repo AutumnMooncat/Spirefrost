8dff9afaa3abf2291cc9dcece613923a26772991
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-29 00:44:32 -0300

    Rework Wristblade. Add Gremlin Visage with old Wristblade effect

--- Builders/Cards/Items/GremlinVisage.cs
+++ Builders/Cards/Items/GremlinVisage.cs
@@ -1,29 +1,29 @@
 ﻿using Deadpan.Enums.Engine.Components.Modding;
-using Spirefrost.Builders.StatusEffects;
+using Spirefrost.Builders.StatusEffects.IconEffects;
 using static Spirefrost.MainModFile;
 using static Spirefrost.SpirefrostUtils.AutoAdd;
 
 namespace Spirefrost.Builders.Cards.Items
 {
-    [ToPoolList(PoolListType.SilentItems)]
-    internal class WristBlade : SpirefrostBuilder
+    [ToPoolList(PoolListType.Items)]
+    internal class GremlinVisage : SpirefrostBuilder
     {
-        internal static string ID => "wristblade";
+        internal static string ID => "gremlinvisage";
 
         internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
 
         internal static object GetBuilder()
         {
             return new CardDataBuilder(MainModFile.instance)
-                .CreateItem(ID, "Wrist Blade")
-                .SetSprites("Items/WristBlade.png", "Items/WristBladeBG.png")
-                .WithValue(50)
-                .SetDamage(2)
+                .CreateItem(ID, "Gremlin Visage")
+                .SetSprites("Items/GremlinVisage.png", "Items/GremlinVisageBG.png")
+                .WithValue(55)
+                .SetDamage(1)
                 .SubscribeToAfterAllBuildEvent(data =>
                 {
-                    data.startWithEffects = new CardData.StatusEffectStacks[]
+                    data.attackEffects = new CardData.StatusEffectStacks[]
                     {
-                        SStack(WhileInHandIncreaseItemDamage.ID, 2)
+                        SStack(Weak.ID, 1)
                     };
                 });
         }
---
+++ Builders/StatusEffects/WhileInHandIncreaseItemDamage.cs
@@ -0,0 +1,26 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class WhileInHandIncreaseItemDamage : SpirefrostBuilder
+    {
+        internal static string ID => "While In Hand Increase Item Damage";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("While In Hand Reduce Attack To Allies", ID)
+                .WithText("While in hand, add <+{a}><keyword=attack> to <Items> in your hand")
+                .SubscribeToAfterAllBuildEvent<StatusEffectWhileInHandX>(data =>
+                {
+                    data.applyToFlags = StatusEffectApplyX.ApplyToFlags.Hand;
+                    data.effectToApply = TryGet<StatusEffectData>("Ongoing Increase Attack");
+                    data.applyConstraints = new TargetConstraint[]
+                    {
+                        MakeConstraint<TargetConstraintIsItem>()
+                    };
+                });
+        }
+    }
+}
