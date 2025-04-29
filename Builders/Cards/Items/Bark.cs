80147250cda62b6768fee6148f4640a474dee511
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-29 13:29:07 -0300

    Rework War Paint. Add Bark with old War Paint effect

--- Builders/Cards/Items/Bark.cs
+++ Builders/Cards/Items/Bark.cs
@@ -1,29 +1,30 @@
 ﻿using Deadpan.Enums.Engine.Components.Modding;
-using Spirefrost.Builders.StatusEffects.IconEffects;
+using Spirefrost.Builders.StatusEffects;
 using static Spirefrost.MainModFile;
 using static Spirefrost.SpirefrostUtils.AutoAdd;
 
 namespace Spirefrost.Builders.Cards.Items
 {
     [ToPoolList(PoolListType.Items)]
-    internal class WarPaint : SpirefrostBuilder
+    internal class Bark : SpirefrostBuilder
     {
-        internal static string ID => "warpaint";
+        internal static string ID => "bark";
 
         internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
 
         internal static object GetBuilder()
         {
             return new CardDataBuilder(MainModFile.instance)
-                .CreateItem(ID, "War Paint")
-                .SetSprites("Items/WarPaint.png", "Items/WarPaintBG.png")
-                .WithValue(55)
-                .SetTraits(TStack("Barrage", 1))
+                .CreateItem(ID, "Sacred Bark")
+                .SetSprites("Items/Bark.png", "Items/BarkBG.png")
+                .WithValue(60)
+                .SetTraits(TStack("Consume", 1))
+                .CanPlayOnHand(true)
                 .SubscribeToAfterAllBuildEvent(data =>
                 {
                     data.attackEffects = new CardData.StatusEffectStacks[]
                     {
-                        SStack(Amplify.ID, 1)
+                        SStack(IncreaseEffectsWithDesc.ID, 1)
                     };
                 });
         }
