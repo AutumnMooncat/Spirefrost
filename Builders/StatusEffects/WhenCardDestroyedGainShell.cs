a6bf26403f1788921d651642990b29a370531e48
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-29 18:23:45 -0300

    Fix Feel No Pain typo

--- Builders/StatusEffects/WhenCardDestroyedGainShell.cs
+++ Builders/StatusEffects/WhenCardDestroyedGainShell.cs
@@ -11,7 +11,7 @@ namespace Spirefrost.Builders.StatusEffects
         internal static object GetBuilder()
         {
             return StatusCopy("When Card Destroyed, Gain Frenzy", ID)
-                .WithText("When a card is destroyed, gain <+{a}><keyword=shell>")
+                .WithText("When a card is destroyed, gain <{a}><keyword=shell>")
                 .SubscribeToAfterAllBuildEvent<StatusEffectApplyXWhenCardDestroyed>(data =>
                 {
                     data.effectToApply = TryGet<StatusEffectData>("Shell");
