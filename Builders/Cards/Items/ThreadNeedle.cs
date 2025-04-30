e4321429695ac85258269f55d6d4c371d835af48
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-04-30 01:22:21 -0300

    Add Thread and Needle

---
+++ Builders/Cards/Items/ThreadNeedle.cs
@@ -0,0 +1,32 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Items
+{
+    [ToPoolList(PoolListType.Items)]
+    internal class ThreadNeedle : SpirefrostBuilder
+    {
+        internal static string ID => "threadneedle";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateItem(ID, "Thread and Needle")
+                .SetSprites("Items/ThreadNeedle.png", "Items/ThreadNeedleBG.png")
+                .WithValue(50)
+                .SetTraits(TStack("Consume", 1))
+                .CanPlayOnHand(true)
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("Instant Add Scrap", 1),
+                        SStack("Shell", 4)
+                    };
+                });
+        }
+    }
+}
