615d49c08ac5cd113e72aa6ea57f03d362613651
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-03 02:34:42 -0300

    Add Teardrop Locket

---
+++ Builders/Cards/Items/TeardropLocket.cs
@@ -0,0 +1,30 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects.IconEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Items
+{
+    [ToPoolList(PoolListType.WatcherItems)]
+    internal class TeardropLocket : SpirefrostBuilder
+    {
+        internal static string ID => "teardroplocket";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateItem(ID, "Teardrop Locket")
+                .SetSprites("Items/TeardropLocket.png", "Items/TeardropLocketBG.png")
+                .WithValue(50)
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack(Energized.ID, 2)
+                    };
+                });
+        }
+    }
+}
