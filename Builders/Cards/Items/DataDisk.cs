aea698336a5051e91353c23f6eebb6ce0e17cd4a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-03 14:02:43 -0300

    Add Data Disk

---
+++ Builders/Cards/Items/DataDisk.cs
@@ -0,0 +1,32 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Items
+{
+    [ToPoolList(PoolListType.DefectItems)]
+    internal class DataDisk : SpirefrostBuilder
+    {
+        internal static string ID => "datadisk";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateItem(ID, "Data Disk")
+                .SetSprites("Items/DataDisk.png", "Items/DataDiskBG.png")
+                .WithValue(55)
+                .CanPlayOnHand(true)
+                .SetTraits(TStack("Consume", 1))
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack(TemporaryFocus.ID, 1)
+                    };
+                });
+        }
+    }
+}
---
+++ Builders/StatusEffects/TemporaryFocus.cs
@@ -0,0 +1,27 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.Traits;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class TemporaryFocus : SpirefrostBuilder
+    {
+        internal static string ID => "Temporary Focus";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("Temporary Pigheaded", ID)
+                .WithCanBeBoosted(true)
+                .WithStackable(true)
+                .SubscribeToAfterAllBuildEvent<StatusEffectTemporaryTrait>(data =>
+                {
+                    data.trait = TryGet<TraitData>(FocusTrait.ID);
+                    data.targetConstraints = new TargetConstraint[]
+                    {
+                        MakeConstraint<TargetConstraintIsUnit>()
+                    };
+                });
+        }
+    }
+}
