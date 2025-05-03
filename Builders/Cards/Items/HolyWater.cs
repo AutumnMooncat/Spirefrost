6b08b579ef34c209b7c611a96ea3af9ee351fca8
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-03 01:47:15 -0300

    Add Retain to Holy Water

--- Builders/Cards/Items/HolyWater.cs
+++ Builders/Cards/Items/HolyWater.cs
@@ -1,4 +1,5 @@
 ﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.Traits;
 
 namespace Spirefrost.Builders.Cards.Items
 {
@@ -15,7 +16,14 @@ namespace Spirefrost.Builders.Cards.Items
                 .SetSprites("Items/HolyWater.png", "Items/HolyWaterBG.png")
                 .WithValue(50)
                 .SetAttackEffect(SStack("Reduce Counter", 3))
-                .SetTraits(TStack("Consume", 1));
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.traits = new System.Collections.Generic.List<CardData.TraitStacks>
+                    {
+                        TStack(RetainTrait.ID, 1),
+                        TStack("Consume", 1)
+                    };
+                });
         }
     }
 }
