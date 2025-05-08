7c2f8664c785b9d018f872c20b6eff51da0a29ef
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-08 03:13:18 -0300

    Rename and rework Red Slaver

--- Builders/Cards/Companions/RedSlaver.cs
+++ Builders/Cards/Companions/RedSlaver.cs
@@ -1,14 +1,15 @@
 ﻿using Deadpan.Enums.Engine.Components.Modding;
 using Spirefrost.Builders.StatusEffects;
+using Spirefrost.Builders.StatusEffects.IconEffects;
 using static Spirefrost.MainModFile;
 using static Spirefrost.SpirefrostUtils.AutoAdd;
 
 namespace Spirefrost.Builders.Cards.Companions
 {
     [ToPoolList(PoolListType.Units)]
-    internal class Slaver : SpirefrostBuilder
+    internal class RedSlaver : SpirefrostBuilder
     {
-        internal static string ID => "slaver";
+        internal static string ID => "redslaver";
 
         internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
 
@@ -17,14 +18,14 @@ namespace Spirefrost.Builders.Cards.Companions
             return new CardDataBuilder(MainModFile.instance)
                 .CreateUnit(ID, "Slaver")
                 .SetSprites("Units/Slaver.png", "Units/SlaverBG.png")
-                .SetStats(6, 3, 5)
+                .SetStats(6, 3, 3)
                 .WithValue(50)
                 .SetTraits(TStack("Longshot", 1))
                 .SubscribeToAfterAllBuildEvent(data =>
                 {
-                    data.startWithEffects = new CardData.StatusEffectStacks[]
+                    data.attackEffects = new CardData.StatusEffectStacks[]
                     {
-                        SStack(OnHitIncreaseCounter.ID, 2)
+                        SStack(Vulnerable.ID, 1)
                     };
                 });
         }
