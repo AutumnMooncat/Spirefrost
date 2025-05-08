683afbc89288fe6484db5635827e9faec04da364
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-08 19:29:44 -0300

    Byrd now Watcher unit

--- Builders/Cards/Companions/Byrd.cs
+++ Builders/Cards/Companions/Byrd.cs
@@ -5,7 +5,7 @@ using static Spirefrost.SpirefrostUtils.AutoAdd;
 
 namespace Spirefrost.Builders.Cards.Companions
 {
-    [ToPoolList(PoolListType.Units)]
+    [ToPoolList(PoolListType.WatcherUnits)]
     internal class Byrd : SpirefrostBuilder
     {
         internal static string ID => "byrd";
