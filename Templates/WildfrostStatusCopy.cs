a20083ccf2d7a67370df323a6e97ed5683fd05f5
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-08 01:18:06 -0300

    Add more templates

---
+++ Templates/WildfrostStatusCopy.cs
@@ -0,0 +1,21 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+
+namespace Spirefrost.Templates
+{
+    internal class WildfrostStatusCopy : SpirefrostBuilder
+    {
+        internal static string ID => null;
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("", ID)
+                .WithText("")
+                .SubscribeToAfterAllBuildEvent<StatusEffectData>(data =>
+                {
+                    
+                });
+        }
+    }
+}
---
+++ Templates/WildfrostStatusNew.cs
@@ -0,0 +1,23 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+
+namespace Spirefrost.Templates
+{
+    internal class WildfrostStatusNew : SpirefrostBuilder
+    {
+        internal static string ID => null;
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new StatusEffectDataBuilder(MainModFile.instance)
+                .Create<StatusEffectData>(ID)
+                .WithText("")
+                .WithCanBeBoosted(true)
+                .SubscribeToAfterAllBuildEvent<StatusEffectData>(data =>
+                {
+                    
+                });
+        }
+    }
+}
