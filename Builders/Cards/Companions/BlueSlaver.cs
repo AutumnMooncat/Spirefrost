6d75efdc25fba583685a04cef5d37c5650e46e2a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-08 12:13:45 -0300

    Add Blue Slaver and Snecko

---
+++ Builders/Cards/Companions/BlueSlaver.cs
@@ -0,0 +1,32 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects.IconEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Companions
+{
+    [ToPoolList(PoolListType.Units)]
+    internal class BlueSlaver : SpirefrostBuilder
+    {
+        internal static string ID => "blueslaver";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateUnit(ID, "Slaver")
+                .SetSprites("Units/SlaverBlue.png", "Units/SlaverBlueBG.png")
+                .SetStats(6, 2, 4)
+                .WithValue(50)
+                .SetTraits(TStack("Longshot", 1), TStack("Pull", 2))
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack(Weak.ID, 1)
+                    };
+                });
+        }
+    }
+}
---
+++ Builders/Cards/Companions/Snecko.cs
@@ -0,0 +1,31 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Companions
+{
+    [ToPoolList(PoolListType.Units)]
+    internal class Snecko : SpirefrostBuilder
+    {
+        internal static string ID => "snecko";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateUnit(ID, "Snecko")
+                .SetSprites("Units/Snecko.png", "Units/SneckoBG.png")
+                .SetStats(7, 0, 5)
+                .WithValue(50)
+                .SetTraits(TStack("Aimless", 1))
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("Haze", 1)
+                    };
+                });
+        }
+    }
+}
