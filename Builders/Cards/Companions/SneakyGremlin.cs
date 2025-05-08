2df0d962c219fe7686944fd3fd3e58040f08f7ff
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-08 01:52:16 -0300

    Rework Sneaky Gremlin

--- Builders/Cards/Companions/SneakyGremlin.cs
+++ Builders/Cards/Companions/SneakyGremlin.cs
@@ -4,7 +4,7 @@ using static Spirefrost.SpirefrostUtils.AutoAdd;
 
 namespace Spirefrost.Builders.Cards.Companions
 {
-    [ToPoolList(PoolListType.Units)]
+    [ToPoolList(PoolListType.SilentUnits)]
     internal class SneakyGremlin : SpirefrostBuilder
     {
         internal static string ID => "sneakygremlin";
@@ -16,13 +16,17 @@ namespace Spirefrost.Builders.Cards.Companions
             return new CardDataBuilder(MainModFile.instance)
                 .CreateUnit(ID, "Sneaky Gremlin")
                 .SetSprites("Units/SneakyGremlin.png", "Units/SneakyGremlinBG.png")
-                .SetStats(2, 2, 0)
+                .SetStats(2, 1, 0)
                 .WithValue(50)
                 .SubscribeToAfterAllBuildEvent(data =>
                 {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("Shroom", 1)
+                    };
                     data.startWithEffects = new CardData.StatusEffectStacks[]
                     {
-                        SStack("Trigger Against When Ally Attacks", 1)
+                        SStack("Trigger When Ally Attacks", 1)
                     };
                 });
         }
