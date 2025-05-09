84a0a776827df0b89bbff1f962fdae05db6f7c29
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-08 21:24:15 -0300

    Add Gremlin Leader

---
+++ Builders/Cards/Companions/GremlinLeader.cs
@@ -0,0 +1,29 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+
+namespace Spirefrost.Builders.Cards.Companions
+{
+    internal class GremlinLeader : SpirefrostBuilder
+    {
+        internal static string ID => "gremlinleader";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateUnit(ID, "Gremlin Leader")
+                .SetSprites("Units/GremlinLeader.png", "Units/GremlinLeaderBG.png")
+                .SetStats(9, 0, 5)
+                .WithValue(50)
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.startWithEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("MultiHit", 2),
+                        SStack(WhileActiveIncreaseAttackToGremlins.ID, 2)
+                    };
+                });
+        }
+    }
+}
---
+++ Builders/StatusEffects/WhileActiveIncreaseAttackToGremlins.cs
@@ -0,0 +1,38 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.Cards.Companions;
+using Spirefrost.Builders.Cards.Items;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class WhileActiveIncreaseAttackToGremlins : SpirefrostBuilder
+    {
+        internal static string ID => "While Active Increase Attack To Gremlins";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("While Active Increase Attack To Allies", ID)
+                .WithText("While active, add <+{a}><keyword=attack> to all <Gremlin> cards")
+                .SubscribeToAfterAllBuildEvent<StatusEffectWhileActiveX>(data =>
+                {
+                    data.applyToFlags = StatusEffectApplyX.ApplyToFlags.Self | StatusEffectApplyX.ApplyToFlags.Allies | StatusEffectApplyX.ApplyToFlags.Hand;
+                    data.applyConstraints = new TargetConstraint[]
+                    {
+                        MakeConstraint<TargetConstraintIsSpecificCard>(t => t.allowedCards = new CardData[]
+                        {
+                            TryGet<CardData>(FatGremlin.ID),
+                            TryGet<CardData>(MadGremlin.ID),
+                            TryGet<CardData>(ShieldGremlin.ID),
+                            TryGet<CardData>(SneakyGremlin.ID),
+                            TryGet<CardData>(GremlinWizard.ID),
+                            TryGet<CardData>(Nob.ID),
+                            TryGet<CardData>(GremlinLeader.ID),
+                            TryGet<CardData>(GremlinHorn.ID),
+                            TryGet<CardData>(GremlinVisage.ID),
+                        })
+                    };
+                });
+        }
+    }
+}
