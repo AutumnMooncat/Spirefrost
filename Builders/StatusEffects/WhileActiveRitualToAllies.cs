a75e6426e9995b9c252443e4b685e8da75c2cef5
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-09 00:39:40 -0300

    Orb Walker now affects all allies

--- Builders/StatusEffects/WhileActiveRitualToAllies.cs
+++ Builders/StatusEffects/WhileActiveRitualToAllies.cs
@@ -4,16 +4,16 @@ using Spirefrost.Builders.StatusEffects.IconEffects;
 
 namespace Spirefrost.Builders.StatusEffects
 {
-    internal class WhileActiveRitualToAlliesInRow : SpirefrostBuilder
+    internal class WhileActiveRitualToAllies : SpirefrostBuilder
     {
-        internal static string ID => "While Active STS Ritual To AlliesInRow";
+        internal static string ID => "While Active STS Ritual To Allies";
 
         internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
 
         internal static object GetBuilder()
         {
-            return StatusCopy("While Active Frenzy To AlliesInRow", ID)
-                .WithText($"While active, add <{{a}}>{MakeKeywordInsert(RitualKeyword.FullID)} to allies in the row")
+            return StatusCopy("While Active Teeth To Allies", ID)
+                .WithText($"While active, add <{{a}}>{MakeKeywordInsert(RitualKeyword.FullID)} to all allies")
                 .SubscribeToAfterAllBuildEvent<StatusEffectWhileActiveX>(data =>
                 {
                     data.effectToApply = TryGet<StatusEffectData>(Ritual.ID);
