3f8b14e7cd5d61153484f5ee7bc6fd0c4d3a6d56
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-09 17:18:04 -0300

    Change effect being copied

--- Builders/StatusEffects/WhenAllyIsHitApplyShellToThem.cs
+++ Builders/StatusEffects/WhenAllyIsHitApplyShellToThem.cs
@@ -10,7 +10,7 @@ namespace Spirefrost.Builders.StatusEffects
 
         internal static object GetBuilder()
         {
-            return StatusCopy("When Ally is Hit Apply Vim To Target", ID)
+            return StatusCopy("When Ally is Hit Heal To Target", ID)
                 .WithText("When an ally is hit, apply <{a}><keyword=shell> to them")
                 .SubscribeToAfterAllBuildEvent<StatusEffectApplyXWhenAllyIsHit>(data =>
                 {
