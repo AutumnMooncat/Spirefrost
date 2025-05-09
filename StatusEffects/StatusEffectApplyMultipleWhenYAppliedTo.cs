3660971492d14bf1985fa29a554e15e99b7d1c60
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-09 20:54:48 -0300

    Add unused effect class

---
+++ StatusEffects/StatusEffectApplyMultipleWhenYAppliedTo.cs
@@ -0,0 +1,36 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using System.Collections;
+using static StatusEffectApplyX;
+
+namespace Spirefrost.StatusEffects
+{
+    internal class StatusEffectApplyMultipleWhenYAppliedTo : StatusEffectApplyXWhenYAppliedTo
+    {
+        public class ApplyData
+        {
+            public StatusEffectData effect;
+
+            public ApplyToFlags flags;
+
+            public TargetConstraint[] constraints;
+        }
+
+        public ApplyData[] applyDatas;
+
+        public override void Init()
+        {
+            base.PostApplyStatus += CompoundRun;
+        }
+
+        public IEnumerator CompoundRun(StatusEffectApply apply)
+        {
+            foreach (var item in applyDatas)
+            {
+                effectToApply = item.effect;
+                applyToFlags = item.flags;
+                applyConstraints = item.constraints;
+                yield return Run(GetTargets(), apply.count);
+            }
+        }
+    }
+}
