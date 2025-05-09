e9baa3bccf81fb218137996dfc69a92112e250e4
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-09 01:19:09 -0300

    Rework Book of Stabbing

--- Builders/Cards/Clunkers/BookOfStabbing.cs
+++ Builders/Cards/Clunkers/BookOfStabbing.cs
@@ -1,4 +1,5 @@
 ﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
 using static Spirefrost.MainModFile;
 using static Spirefrost.SpirefrostUtils.AutoAdd;
 
@@ -16,7 +17,7 @@ namespace Spirefrost.Builders.Cards.Clunkers
             return new CardDataBuilder(MainModFile.instance)
                 .CreateUnit(ID, "Book of Stabbing")
                 .SetSprites("Units/BookOfStabbing.png", "Units/BookOfStabbingBG.png")
-                .SetStats(null, null, 0)
+                .SetStats(null, 3, 0)
                 .WithCardType("Clunker")
                 .WithValue(50)
                 .SubscribeToAfterAllBuildEvent(data =>
@@ -24,7 +25,7 @@ namespace Spirefrost.Builders.Cards.Clunkers
                     data.startWithEffects = new CardData.StatusEffectStacks[]
                     {
                         SStack("Scrap", 1),
-                        SStack("While Active Frenzy To AlliesInRow", 1)
+                        SStack(TriggerAgainstWhenVulnApplied.ID, 1)
                     };
                 });
         }
---
+++ Builders/StatusEffects/TriggerAgainstWhenVulnApplied.cs
@@ -0,0 +1,23 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.Keywords;
+using Spirefrost.Builders.StatusEffects.IconEffects;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class TriggerAgainstWhenVulnApplied : SpirefrostBuilder
+    {
+        internal static string ID => null;
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("Trigger Against When Snow Applied", ID)
+                .WithTextInsert(MakeKeywordInsert(VulnerableKeyword.FullID))
+                .SubscribeToAfterAllBuildEvent<StatusEffectTriggerWhenStatusApplied>(data =>
+                {
+                    data.targetStatus = TryGet<StatusEffectData>(Vulnerable.ID);
+                });
+        }
+    }
+}
