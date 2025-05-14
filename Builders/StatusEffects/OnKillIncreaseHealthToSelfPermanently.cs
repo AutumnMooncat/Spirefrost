4ded765a801cfb3e79e27751b859a9ae2641f00b
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-14 15:46:33 -0300

    Add effect to potentially try for Ironclad, currently unused

---
+++ Builders/StatusEffects/OnKillIncreaseHealthToSelfPermanently.cs
@@ -0,0 +1,24 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.StatusEffects;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class OnKillIncreaseHealthToSelfPermanently : SpirefrostBuilder
+    {
+        internal static string ID => "On Kill Increase Health To Self Permanently";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new StatusEffectDataBuilder(MainModFile.instance)
+                .Create<StatusEffectFeed>(ID)
+                .WithText("On kill, add +{a}<keyword=health> to self permanently")
+                .WithCanBeBoosted(false)
+                .SubscribeToAfterAllBuildEvent<StatusEffectFeed>(data =>
+                {
+
+                });
+        }
+    }
+}
---
+++ StatusEffects/StatusEffectFeed.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spirefrost.StatusEffects
+{
+    internal class StatusEffectFeed : StatusEffectApplyX
+    {
+        public override void Init()
+        {
+            base.OnEntityDestroyed += CheckDestroy;
+            effectToApply = MainModFile.instance.TryGet<StatusEffectData>("Increase Max Health");
+            applyToFlags = ApplyToFlags.Self;
+            doPing = true;
+        }
+
+        public override bool RunEntityDestroyedEvent(Entity entity, DeathType deathType)
+        {
+            if (entity.lastHit != null)
+            {
+                return entity.lastHit.attacker == target;
+            }
+
+            return false;
+        }
+
+        public IEnumerator CheckDestroy(Entity entity, DeathType deathType)
+        {
+            int amount = GetAmount();
+            if (amount > 0)
+            {
+                CardData deckVersion = References.PlayerData.inventory.deck.list.Where(data => data.id == target.data.id).FirstOrDefault();
+                if (deckVersion)
+                {
+                    deckVersion.hp += amount;
+                    if (deckVersion.hp <= 0)
+                    {
+                        deckVersion.hp = 1;
+                    }
+                }
+            }
+            return Run(new List<Entity>() { target });
+        }
+    }
+}
