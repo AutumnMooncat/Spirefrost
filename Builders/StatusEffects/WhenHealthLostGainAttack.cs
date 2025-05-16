a489eb21972eef38aca9354d45aec0adb2878608
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-16 02:15:13 -0300

    Add currently unused effect

---
+++ Builders/StatusEffects/WhenHealthLostGainAttack.cs
@@ -0,0 +1,22 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class WhenHealthLostGainAttack : SpirefrostBuilder
+    {
+        internal static string ID => "When Health Lost Gain Attack";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("When Health Lost Apply Equal Attack To Self And Allies", ID)
+                .WithText("When <keyword=health> lost, gain <+{a}><keyword=attack>")
+                .SubscribeToAfterAllBuildEvent<StatusEffectApplyXWhenHealthLost>(data =>
+                {
+                    data.applyEqualAmount = false;
+                    data.applyToFlags = StatusEffectApplyX.ApplyToFlags.Self;
+                });
+        }
+    }
+}
