233160d7548335438252b0214f4664b701e1e925
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-16 00:57:31 -0300

    Add patch to make cards in the draw/discard piles enabled so status effects work from there, this likely causes some problems elsewhere that will have to be patched

---
+++ Patches/CardPocketPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace Spirefrost.Patches
+{
+    [HarmonyPatch(typeof(CardPocket), "CardAdded")]
+    internal class CardPocketPatch
+    {
+        static void Postfix(Entity entity)
+        {
+            entity.enabled = true;
+        }
+    }
+}
