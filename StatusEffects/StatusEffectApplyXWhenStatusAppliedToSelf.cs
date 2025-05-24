9ba184d33e81626b0a47d139c49942cc74455231
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-23 21:04:46 -0300

    Add additional applier restrictions

--- StatusEffects/StatusEffectApplyXWhenStatusAppliedToSelf.cs
+++ StatusEffects/StatusEffectApplyXWhenStatusAppliedToSelf.cs
@@ -6,19 +6,47 @@ namespace Spirefrost.StatusEffects
     {
         public bool selfCanBeApplier;
 
+        public bool allyCanBeApplier = true;
+
+        public bool enemyCanBeApplier = true;
+
+        public bool itemCanBeApplier = true;
+
         public override void Init()
         {
-            base.PostApplyStatus += Check;
+            base.PostApplyStatus += DoApply;
         }
 
         public override bool RunPostApplyStatusEvent(StatusEffectApply apply)
         {
             if (target.enabled && ShouldApply(apply.effectData))
             {
-                return apply.target == target && (apply.applier != target || selfCanBeApplier);
+                return apply.target == target && ApplierCheck(apply.applier);
             }
 
             return false;
+        }
+
+        private bool ApplierCheck(Entity applier)
+        {
+            if (applier == target)
+            {
+                return selfCanBeApplier;
+            }
+
+            if (applier.data.IsItem)
+            {
+                return itemCanBeApplier;
+            }
+
+            if (applier.owner == target.owner)
+            {
+                return allyCanBeApplier;
+            }
+            else
+            {
+                return enemyCanBeApplier;
+            }
         }
 
         private bool ShouldApply(StatusEffectData apply)
@@ -26,7 +54,7 @@ namespace Spirefrost.StatusEffects
             return apply != null && apply.isStatus && apply.visible;
         }
 
-        public IEnumerator Check(StatusEffectApply apply)
+        public IEnumerator DoApply(StatusEffectApply apply)
         {
             return Run(GetTargets());
         }
