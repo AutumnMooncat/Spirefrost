d52b53ee194bbb92b867dd57b4791438a610577a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-05-31 17:43:55 -0300

    Give Looter an extra counter

--- Builders/Cards/Companions/Looter.cs
+++ Builders/Cards/Companions/Looter.cs
@@ -1,5 +1,6 @@
 ﻿using Deadpan.Enums.Engine.Components.Modding;
 using Spirefrost.Builders.StatusEffects;
+using Spirefrost.Builders.StatusEffects.IconEffects;
 using static Spirefrost.MainModFile;
 using static Spirefrost.SpirefrostUtils.AutoAdd;
 
@@ -15,15 +16,16 @@ namespace Spirefrost.Builders.Cards.Companions
         internal static object GetBuilder()
         {
             return new CardDataBuilder(MainModFile.instance)
-                .CreateUnit(ID, "Looter")
-                .SetSprites("Units/Looter.png", "Units/LooterBG.png")
-                .SetStats(5, 4, 3)
+                .CreateUnit(ID, "Looter & Mugger")
+                .SetSprites("Units/LooterMugger.png", "Units/LooterBG.png")
+                .SetStats(5, 2, 3)
                 .WithValue(50)
                 .SubscribeToAfterAllBuildEvent(data =>
                 {
                     data.startWithEffects = new CardData.StatusEffectStacks[]
                     {
                         SStack("On Kill Apply Gold To Self", 5),
+                        SStack(ExtraCounter.ID, 4),
                         SStack(OnKillTriggerAgain.ID, 1)
                     };
                 });
