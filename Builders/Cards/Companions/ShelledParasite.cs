647068f26c1351db09bedb93b0dadac830bfb336
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-07 03:36:17 -0300

    Add Shelled Parasite for Ironclad

---
+++ Builders/Cards/Companions/ShelledParasite.cs
@@ -0,0 +1,32 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Companions
+{
+    [ToPoolList(PoolListType.IroncladUnits)]
+    internal class ShelledParasite : SpirefrostBuilder
+    {
+        internal static string ID => "shelledparasite";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateUnit(ID, "Shelled Parasite")
+                .SetSprites("Units/ShelledParasite.png", "Units/ShelledParasiteBG.png")
+                .SetStats(5, 2, 4)
+                .WithValue(50)
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.startWithEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack(OnHitEqualHealToSelf.ID, 1),
+                        SStack("Shell", 3)
+                    };
+                });
+        }
+    }
+}
---
+++ Builders/StatusEffects/OnHitEqualHealToSelf.cs
@@ -0,0 +1,21 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class OnHitEqualHealToSelf : SpirefrostBuilder
+    {
+        internal static string ID => "On Hit Equal Heal To Self";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("On Hit Equal Heal To FrontAlly", ID)
+                .WithText("Restore <keyword=health> equal to damage dealt")
+                .SubscribeToAfterAllBuildEvent<StatusEffectApplyXOnHit>(data =>
+                {
+                    data.applyToFlags = StatusEffectApplyX.ApplyToFlags.Self;
+                });
+        }
+    }
+}
