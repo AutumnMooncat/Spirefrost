d1112c6bef6d382c3dee5bd07bd2ffdbd3a84dd7
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-08 19:43:51 -0300

    Add backup effect for use on Spire Growth

---
+++ Builders/StatusEffects/OnTurnEndConstrictedDamageFrontEnemy.cs
@@ -0,0 +1,49 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.Icons;
+using Spirefrost.StatusEffects;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class OnTurnEndConstrictedDamageFrontEnemy : SpirefrostBuilder
+    {
+        internal static string ID => "On Turn End Constricted Damage Front Enemy";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new StatusEffectDataBuilder(MainModFile.instance)
+                .Create<StatusEffectApplyXAfterTurn>(ID)
+                .WithText("While active, deal <{a}> damage to the front enemy each turn")
+                .WithCanBeBoosted(true)
+                .SubscribeToAfterAllBuildEvent<StatusEffectApplyXAfterTurn>(data =>
+                {
+                    data.ignoreSilence = false;
+                    data.effectToApply = TryGet<StatusEffectData>(ConstrictedDamage.ID);
+                    data.applyToFlags = StatusEffectApplyX.ApplyToFlags.FrontEnemy;
+                });
+        }
+    }
+
+    internal class ConstrictedDamage : SpirefrostBuilder
+    {
+        internal static string ID => "Constricted Damage";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new StatusEffectDataBuilder(MainModFile.instance)
+                .Create<StatusEffectInstantDamage>(ID)
+                .WithCanBeBoosted(false)
+                .WithStackable(false)
+                .SubscribeToAfterAllBuildEvent<StatusEffectInstantDamage>(data =>
+                {
+                    data.doesDamage = true;
+                    data.countsAsHit = true;
+                    data.canRetaliate = false;
+                    data.damageType = ConstrictedIcon.DamageID;
+                });
+        }
+    }
+}
---
+++ StatusEffects/StatusEffectApplyXAfterTurn.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Spirefrost.StatusEffects
+{
+    internal class StatusEffectApplyXAfterTurn : StatusEffectApplyX
+    {
+        public bool subbed;
+
+        public bool primed;
+
+        public bool ignoreSilence = true;
+
+        public override void Init()
+        {
+            OnTurnEnd += PostTurn;
+            Events.OnPostProcessUnits += Prime;
+            subbed = true;
+        }
+
+        public void OnDestroy()
+        {
+            Unsub();
+        }
+
+        public void Unsub()
+        {
+            if (subbed)
+            {
+                Events.OnPostProcessUnits -= Prime;
+                subbed = false;
+            }
+        }
+
+        public void Prime(Character character)
+        {
+            primed = true;
+            Unsub();
+        }
+
+        public override bool TargetSilenced()
+        {
+            if (ignoreSilence)
+            {
+                return false;
+            }
+            return base.TargetSilenced();
+        }
+
+        public override int GetAmount()
+        {
+            if (!target || (target.silenced && !ignoreSilence))
+            {
+                return 0;
+            }
+
+            if (!canBeBoosted)
+            {
+                return count;
+            }
+
+            return Mathf.Max(0, Mathf.RoundToInt((count + target.effectBonus) * target.effectFactor));
+        }
+
+        public override bool RunTurnEndEvent(Entity entity)
+        {
+            if (primed && target.enabled && Battle.IsOnBoard(target))
+            {
+                return entity == target;
+            }
+
+            return false;
+        }
+
+        public IEnumerator PostTurn(Entity entity)
+        {
+            yield return Run(GetTargets());
+        }
+    }
+}
