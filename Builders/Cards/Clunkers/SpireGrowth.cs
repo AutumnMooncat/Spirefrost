e59d52abe9d60632d710ef5d1ea61cdc5c98151e
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-08 00:03:54 -0300

    Add Spire Growth for Ironclad

---
+++ Builders/Cards/Clunkers/SpireGrowth.cs
@@ -0,0 +1,31 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Clunkers
+{
+    [ToPoolList(PoolListType.IroncladUnits)]
+    internal class SpireGrowth : SpirefrostBuilder
+    {
+        internal static string ID => "spiregrowth";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateUnit(ID, "Spire Growth")
+                .SetSprites("Units/SpireGrowth.png", "Units/SpireGrowthBG.png")
+                .SetStats(8, 2, 3)
+                .WithValue(50)
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.startWithEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("When Hit Damage To EnemiesInRow", 2)
+                    };
+                });
+        }
+    }
+}
