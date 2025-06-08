75723cf63494ed8b643ee139f46b0da9929478f0
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-08 00:04:13 -0300

    Add Snake Plant for Silent

---
+++ Builders/Cards/Clunkers/SnakePlant.cs
@@ -0,0 +1,31 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Clunkers
+{
+    [ToPoolList(PoolListType.SilentUnits)]
+    internal class SnakePlant : SpirefrostBuilder
+    {
+        internal static string ID => "snakeplant";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateUnit(ID, "Snake Plant")
+                .SetSprites("Units/SnakePlant.png", "Units/SnakePlantBG.png")
+                .SetStats(8, 1, 5)
+                .WithValue(50)
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.startWithEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("When Hit Apply Shell To Self", 2),
+                        SStack("MultiHit", 2)
+                    };
+                });
+        }
+    }
+}
