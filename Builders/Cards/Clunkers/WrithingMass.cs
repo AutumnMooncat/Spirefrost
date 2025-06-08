c08deab56175602e9ca133e5a3690c838ccb73b1
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-08 00:03:08 -0300

    Add Writhing Mass for Defect

---
+++ Builders/Cards/Clunkers/WrithingMass.cs
@@ -0,0 +1,31 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Clunkers
+{
+    [ToPoolList(PoolListType.DefectUnits)]
+    internal class WrithingMass : SpirefrostBuilder
+    {
+        internal static string ID => "writhingmass";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateUnit(ID, "Writhing Mass")
+                .SetSprites("Units/WrithingMass.png", "Units/WrithingMassBG.png")
+                .SetStats(8, 5, 8)
+                .WithValue(50)
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.startWithEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("When Hit Reduce Counter To Self", 2)
+                    };
+                });
+        }
+    }
+}
