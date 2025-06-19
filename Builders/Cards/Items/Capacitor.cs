fa0c34272e6c6bfd052d3dfd6d4ddd9d2e6d4783
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-19 03:50:49 -0300

    Add Capacitor

---
+++ Builders/Cards/Items/Capacitor.cs
@@ -0,0 +1,33 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Items
+{
+    [ToPoolList(PoolListType.DefectItems)]
+    internal class Capacitor : SpirefrostBuilder
+    {
+        internal static string ID => "capacitor";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateItem(ID, "Runic Capacitor")
+                .SetSprites("Items/Capacitor.png", "Items/CapacitorBG.png")
+                .WithValue(60)
+                .CanPlayOnHand(true)
+                .SetTraits(TStack("Consume", 1))
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("Increase Max Counter", 1),
+                        SStack(InstantGainBarrage.ID, 1)
+                    };
+                });
+        }
+    }
+}
---
+++ Builders/StatusEffects/InstantGainBarrage.cs
@@ -0,0 +1,28 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class InstantGainBarrage : SpirefrostBuilder
+    {
+        internal static string ID => "Instant Gain Barrage";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("Instant Gain Aimless", ID)
+                .WithText("Add <keyword=barrage> to the target")
+                .SubscribeToAfterAllBuildEvent<StatusEffectApplyXInstant>(data =>
+                {
+                    data.effectToApply = TryGet<StatusEffectData>("Temporary Barrage");
+                    data.targetConstraints = new TargetConstraint[]
+                    {
+                        MakeConstraint<TargetConstraintHasTrait>(c => {
+                            c.trait = TryGet<TraitData>("Barrage");
+                            c.not = true;
+                        })
+                    };
+                });
+        }
+    }
+}
