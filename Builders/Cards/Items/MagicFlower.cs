4a184293f636d7d0ffbfe509bdcf90967ff29afe
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-19 03:48:11 -0300

    Add Magic Flower

---
+++ Builders/Cards/Items/MagicFlower.cs
@@ -0,0 +1,32 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Items
+{
+    [ToPoolList(PoolListType.IroncladItems)]
+    internal class MagicFlower : SpirefrostBuilder
+    {
+        internal static string ID => "magicflower";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateItem(ID, "Magic Flower")
+                .SetSprites("Items/MagicFlower.png", "Items/MagicFlowerBG.png")
+                .WithValue(60)
+                .CanPlayOnHand(true)
+                .SetTraits(TStack("Consume", 1))
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack(InstantGainFlourish.ID, 1)
+                    };
+                });
+        }
+    }
+}
---
+++ Builders/StatusEffects/TemporaryFlourish.cs
@@ -0,0 +1,49 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class TemporaryFlourish : SpirefrostBuilder
+    {
+        internal static string ID => "Temporary Flourish";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("Temporary Barrage", ID)
+                .SubscribeToAfterAllBuildEvent<StatusEffectTemporaryTrait>(data =>
+                {
+                    data.trait = TryGet<TraitData>("Heartburn");
+                    data.targetConstraints = new TargetConstraint[]
+                    {
+                        MakeConstraint<TargetConstraintHasHealth>()
+                    };
+                });
+        }
+    }
+
+    internal class InstantGainFlourish : SpirefrostBuilder
+    {
+        internal static string ID => "Instant Gain Flourish";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("Instant Gain Aimless", ID)
+                .WithText("Add <keyword=heartburn> to the target")
+                .SubscribeToAfterAllBuildEvent<StatusEffectApplyXInstant>(data =>
+                {
+                    data.effectToApply = TryGet<StatusEffectData>(TemporaryFlourish.ID);
+                    data.targetConstraints = new TargetConstraint[]
+                    {
+                        MakeConstraint<TargetConstraintHasHealth>(),
+                        MakeConstraint<TargetConstraintHasTrait>(c => {
+                            c.trait = TryGet<TraitData>("Heartburn");
+                            c.not = true;
+                        })
+                    };
+                });
+        }
+    }
+}
