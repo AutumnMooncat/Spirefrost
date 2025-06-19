2d32f1fd320dc7f928b805ed5f397ef35de418fc
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-19 04:45:46 -0300

    Add Kite

---
+++ Builders/Cards/Items/Kite.cs
@@ -0,0 +1,33 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Items
+{
+    [ToPoolList(PoolListType.SilentItems)]
+    internal class Kite : SpirefrostBuilder
+    {
+        internal static string ID => "kite";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateItem(ID, "Hovering Kite")
+                .SetSprites("Items/Kite.png", "Items/KiteBG.png")
+                .WithValue(60)
+                .CanPlayOnHand(true)
+                .SetTraits(TStack("Consume", 1))
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("Reduce Max Counter", 1),
+                        SStack("Instant Gain Aimless", 1)
+                    };
+                });
+        }
+    }
+}
