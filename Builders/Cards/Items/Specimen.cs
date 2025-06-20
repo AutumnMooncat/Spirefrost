c653e826bf494bafda0547ae28d06d4aacb08668
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-06-20 15:31:33 -0300

    Cut Snecko Skull, add Specimen to replace it

---
+++ Builders/Cards/Items/Specimen.cs
@@ -0,0 +1,32 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using Spirefrost.Builders.StatusEffects;
+using static Spirefrost.MainModFile;
+using static Spirefrost.SpirefrostUtils.AutoAdd;
+
+namespace Spirefrost.Builders.Cards.Items
+{
+    [ToPoolList(PoolListType.SilentItems)]
+    internal class Specimen : SpirefrostBuilder
+    {
+        internal static string ID => "specimen";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new CardDataBuilder(MainModFile.instance)
+                .CreateItem(ID, "The Specimen")
+                .SetSprites("Items/Specimen.png", "Items/SpecimenBG.png")
+                .WithValue(50)
+                .SetDamage(0)
+                .SubscribeToAfterAllBuildEvent(data =>
+                {
+                    data.attackEffects = new CardData.StatusEffectStacks[]
+                    {
+                        SStack("Shroom", 2),
+                        SStack(InstantApplyCurrentShroomToRandomEnemy.ID, 1)
+                    };
+                });
+        }
+    }
+}
---
+++ Builders/StatusEffects/InstantApplyCurrentShroomToRandomEnemy.cs
@@ -0,0 +1,45 @@
+﻿using Deadpan.Enums.Engine.Components.Modding;
+using UnityEngine;
+
+namespace Spirefrost.Builders.StatusEffects
+{
+    internal class InstantApplyCurrentShroomToRandomEnemy : SpirefrostBuilder
+    {
+        internal static string ID => "Instant Apply Current Shroom To Random Enemy";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return StatusCopy("Instant Apply Current Attack To Random Ally", ID)
+                .WithText("Copy the target's <keyword=shroom> to a random enemy")
+                .SubscribeToAfterAllBuildEvent<StatusEffectApplyXInstant>(data =>
+                {
+                    data.effectToApply = TryGet<StatusEffectData>(InstantApplyCurrentShroomToRandomEnemyCallback.ID);
+                    data.applyToFlags = StatusEffectApplyX.ApplyToFlags.Applier;
+                    ScriptableCurrentStatus script = ScriptableObject.CreateInstance<ScriptableCurrentStatus>();
+                    data.scriptableAmount = script;
+                });
+        }
+    }
+
+    internal class InstantApplyCurrentShroomToRandomEnemyCallback : SpirefrostBuilder
+    {
+        internal static string ID => "Instant Apply Current Shroom To Random Enemy (Callback)";
+
+        internal static string FullID => Extensions.PrefixGUID(ID, MainModFile.instance);
+
+        internal static object GetBuilder()
+        {
+            return new StatusEffectDataBuilder(MainModFile.instance)
+                .Create<StatusEffectApplyXInstant>(ID)
+                .WithCanBeBoosted(false)
+                .WithStackable(false)
+                .SubscribeToAfterAllBuildEvent<StatusEffectApplyXInstant>(data =>
+                {
+                    data.effectToApply = TryGet<StatusEffectData>("Shroom");
+                    data.applyToFlags = StatusEffectApplyX.ApplyToFlags.RandomEnemy;
+                });
+        }
+    }
+}
